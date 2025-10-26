3813ca51856fcf89f0168a3d928d4c90d2b5ba3c
浅语 <2132895295@qq.com>
2025-10-26 13:01:13 +0800

    Optimize project path

--- src/main/java/aysta3045/ChineseDelight/common/registry/ModItems.java
+++ src/main/java/aysta3045/ChineseDelight/common/registry/ModItems.java
@@ -17,7 +17,4 @@ public class ModItems
     public static final RegistryObject<Item> HOT_DRY_NOODLES = ITEMS.register("hot_dry_noodles",
             HotDryNoodlesItem::new);
 
-    // 大瓶冰红茶(man!What can I say?)
-    public static final RegistryObject<Item> A_LARGE_BOTTLE_OF_ICED_BLACK_TEA= ITEMS.register("a_large_bottle_of_iced_black_tea",
-            ALargeBottleOfIcedBlackTeaItem::new);
 }
