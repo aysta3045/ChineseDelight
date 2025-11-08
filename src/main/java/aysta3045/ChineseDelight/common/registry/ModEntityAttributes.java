a5114aa2d2fcf4a8f42274cd85ca96230bdb84f9
浅语 <2132895295@qq.com>
2025-11-09 00:06:41 +0800

    update

--- src/main/java/aysta3045/ChineseDelight/common/registry/ModEntityAttributes.java
+++ src/main/java/aysta3045/ChineseDelight/common/registry/ModEntityAttributes.java
@@ -1,7 +1,8 @@
 package aysta3045.ChineseDelight.common.registry;
 
 import aysta3045.ChineseDelight.ChineseDelight;
-import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,17 +12,10 @@ public class ModEntityAttributes {
 
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
-        // 注册中华美食鸭的属性，使用原版鸡的属性作为基础
-        event.put(ModEntityTypes.CHINESE_DELIGHT_DUCK.get(),
-                Chicken.createAttributes().build());
-
-        // 如果你想要自定义属性，可以使用以下方式：
-        /*
         event.put(ModEntityTypes.CHINESE_DELIGHT_DUCK.get(),
                 AttributeSupplier.builder()
                         .add(Attributes.MAX_HEALTH, 4.0D)  // 生命值
                         .add(Attributes.MOVEMENT_SPEED, 0.25D)  // 移动速度
                         .build());
-        */
     }
 }
