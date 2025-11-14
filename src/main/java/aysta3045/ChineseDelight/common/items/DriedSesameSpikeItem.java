3c3f3ebd4051591681a0c522be47ef1026a605b8
浅语 <2132895295@qq.com>
2025-11-15 00:32:37 +0800

    添加干芝麻穗

---
+++ src/main/java/aysta3045/ChineseDelight/common/items/DriedSesameSpikeItem.java
@@ -0,0 +1,9 @@
+package aysta3045.ChineseDelight.common.items;
+
+import net.minecraft.world.item.Item;
+
+public class DriedSesameSpikeItem extends Item {
+    public DriedSesameSpikeItem(Properties properties) {
+        super(properties);
+    }
+}
